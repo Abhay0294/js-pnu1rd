035d03496547b47d81d34c17dcd096e45d27d095
Abhay0294 <abhaypratap349@gmail.com>
2022-08-10 22:39:33 +0530

    2 files modified

--- Test-Components/test-file.js
+++ Test-Components/test-file.js
@@ -1,6 +1,9 @@
-import Logger from '././Custom-Implemenations/Logger/logger-Singleton-Class';
+import Logger from './../Custom-Implementations/Logger/logger-Singleton-Class';
 
-export default class PrintHello {
-  x = new Logger();
-  print = () => console.log('$$$$$$$$$$', x);
+export default class TestClass {
+  customConsole3 = new Logger();
+
+  print() {
+    this.customConsole3._log('From test-class');
+  }
 }
--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 // Import stylesheets
 import './style';
 import Logger from './Custom-Implementations/Logger/logger-Singleton-Class';
-import PrintHello from './Test-Components/test-file';
+import TestClass from './Test-Components/test-file';
 
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1 align='center'>Come and Lets play with Singleton classes</h1>`;
 
-const console = new Logger();
+const customConsole = new Logger();
+const testClassObject = new TestClass();
 
-console._log('Hello');
-console._log('Abhay');
-console._log('Kuch bhi');
-console.log('RRRRRRRR', PrintHello.print);
+customConsole._log('Hello');
+customConsole._log('Abhay');
+customConsole._log('Kuch bhi');
+testClassObject.print();
